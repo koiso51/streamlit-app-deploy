d65356c56a012b561723207621e76b6c4c7e102b
koiso51 <kandai.oiso@gmail.com>
2025-09-17 19:41:48 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+env\Scripts\activate.bat
+
+pip install openai
+
+
+
+
+pip install --upgrade pip
+
+pip install streamlit==1.41.1
+
+import streamlit as st
+
+st.title(”Hello Streamlit!”)
+
+import streamlit as st
+
+st.title(”Hello Streamlit!”)
+         
+         
+
+
+import streamlit as st
+
+st.title(”Hello Streamlit!”)
+
+
+st.write("Welcome to your first Streamlit app!")
+
+         
